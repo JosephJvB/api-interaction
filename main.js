fd7f905bfeb5a2b5430359b48ecb3e3066a23dbc
Joe <jo-van-bo@hotmail.com>
2017-08-25 10:49:02 +1200

    added a few notes

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ var sourceContainer = docuement.getElementById("source");  //global vars for htm
 var transContainer = document.getElementById("translation");
 
 var button = document.getElementById("butt");
-button.addEventListener('click', function() {
-  var getQuote = new XMLHttpRequest(); //sends request to the API resource, telling function to read API info in JSON. calls addHTML fucntion
+button.addEventListener('click', function() { //responds to click event
+  var getQuote = new XMLHttpRequest(); //request to the API resource, telling function to read API info in JSON.
   getQuote.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs');
   getQuote.onload = function() {
-    var newQuote = JSON.parse(getQuote.responseText);
+    var newQuote = JSON.parse(getQuote.responseText);  //attempt to alter html elements with recieved information.
     sourceContainer.innerHTML = newQuote.source;
     transContainer.innerHTML = newQuote.translation;
   };
-  getQuote.send();
+  getQuote.send();  // sends request
 });
